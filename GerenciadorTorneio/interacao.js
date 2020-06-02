c044589863ccb79ecf0769623b8cfd78314fbcad
Davi Arrais <daviarrais@hotmail.com>
2020-06-02 20:58:19 -0300

    alteração

---
+++ GerenciadorTorneio/interacao.js
@@ -0,0 +1,8 @@
+function incluirTime(){
+    var t = window.document.getElementById("time")
+    var time = String(t.value)
+    var timeTr = document.createElement("tr")
+    timeTr.classList.add("time")
+
+    
+}
